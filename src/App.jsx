d83e40d5996731a8e435b0a31a5525fa79f37313
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-09-11 03:26:14 +0100

    setting up project folder

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([{}]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
