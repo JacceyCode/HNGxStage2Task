1e4b5a4a8236fadf9b4cdd9f6704bd31467b4de1
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-10-18 23:29:56 +0100

    .

--- src/features/Hero.jsx
+++ src/features/Hero.jsx
@@ -4,7 +4,7 @@ import PaginationBox from "./PaginationBox";
 
 function Hero() {
   return (
-    <section className="flex min-h-screen flex-col gap-20 bg-[url('./images/Poster.png')] bg-cover bg-center bg-no-repeat font-bold text-white md:gap-40">
+    <section className="flex min-h-screen flex-col gap-20 bg-[url('/images/Poster.png')] bg-cover bg-center bg-no-repeat font-bold text-white md:gap-40">
       <Header />
       <section className="flex items-center justify-between px-2 md:pl-10 md:pr-2 lg:pl-24 lg:pr-10">
         <DescriptionBox />
