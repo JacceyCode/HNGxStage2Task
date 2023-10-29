1cf85e925e007d8550830ebf682e3132cff26569
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-10-29 20:28:42 +0100

    changed scrollToTup icon

--- src/ui/ScrollToTop.jsx
+++ src/ui/ScrollToTop.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FaAngleUp } from "react-icons/fa";
+import { FaArrowUpLong } from "react-icons/fa6";
 
 function ScrollToTop() {
   const [showTopBtn, setShowTopBtn] = useState(false);
@@ -19,8 +19,8 @@ function ScrollToTop() {
   return (
     <>
       {showTopBtn && (
-        <FaAngleUp
-          className="fixed bottom-2 right-1 z-10 mx-auto h-8 w-8 animate-bounce cursor-pointer rounded-full border-4 border-black bg-red-700 hover:border-red-700 hover:bg-black hover:text-red-700 lg:right-8 lg:h-12 lg:w-12"
+        <FaArrowUpLong
+          className="fixed bottom-2 right-1 z-10 mx-auto h-8 w-8 animate-bounce cursor-pointer rounded-full border-4 border-black bg-red-700 p-1 hover:border-red-700 hover:bg-black hover:text-red-700 lg:right-8 lg:h-12 lg:w-12"
           onClick={goToTop}
         />
       )}
